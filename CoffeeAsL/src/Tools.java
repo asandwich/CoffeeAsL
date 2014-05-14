a093091a441297468fba5dee2cc618c202d43bdc
Christian <Christian@Christian-PC>
2014-05-13 23:11:46 -0500

    Moved the Tools java file

--- CoffeeAsL/src/Tools.java
+++ CoffeeAsL/src/Tools.java
@@ -1,5 +1,17 @@
 
 
-public class Tools {
+import java.util.HashMap;
 
+public class Tools {
+	static HashMap<String, Integer> properties = new HashMap<String,Integer>();
+	
+	
+	public static Integer getProperty(String string) {
+		return properties.get(string);
+	}
+
+	public static void readPropertiesFile()
+	{
+		
+	}
 }
