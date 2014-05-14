6a1d2f6417fccd63a787a2c8ddec0d07c11b242a
Christian <Christian@Christian-PC>
2014-05-13 23:27:35 -0500

    Moved tools around.

--- CoffeeAsL/src/interpreter/Parser.java
+++ CoffeeAsL/src/interpreter/Parser.java
@@ -1,6 +1,8 @@
 package interpreter;
 
 public class Parser {
+	//Input String[] 
+	
 	//Operation format 
 	//Integer Integer Integer; <--This is an instruction
 	//Integer Integer Integer; <--This is an instruction
